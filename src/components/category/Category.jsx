a97287eea2fad4db3b6c3d30e47279a66676def8
GabrielOchieng <ogingagabriel@gmail.com>
2024-02-13 21:55:32 +0300

    categories setup

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -10,68 +10,86 @@ import "./category.css";
 
 const Category = () => {
   return (
-    <div className="d-flex flex-column align-items-center pt-5">
-      <div className="upperdiv">
+    <div className=" d-flex flex-column align-items-center pt-5">
+      <div className="upperdiv text-center">
         <p>Shop by category</p>
         <h3>Shop by category</h3>
         <hr />
       </div>
-      <div className="lowerdiv d-flex flex-wrap pt-4">
+      <div className="lowerdiv container d-flex gap-3 flex-wrap pt-4">
         <div
-          className="section p-3 m-2 d-flex flex-column align-items-center gap-3 bg-secondary-subtle
+          className="section p-3  d-flex flex-column align-items-center gap-3 bg-secondary-subtle
 "
         >
           <div className="image">
             <img src={women} className="img-fluid" alt="shopping item" />
           </div>
-          <div className="words">
-            <h4>WOMEN'S WEAR</h4>
-            <h5>20 PRODUCTS</h5>
+          <div
+            className="words text-center w-100 p-2 bg-light-subtle
+"
+          >
+            <h4 className="fs-6 fw-bolder">WOMEN'S WEAR</h4>
+            <h5 className="fs-6 fw-normal">20 PRODUCTS</h5>
           </div>
         </div>
-        <div className="section p-3 m-2 d-flex flex-column align-items-center gap-3 bg-secondary-subtle">
+        <div className="section p-3  d-flex flex-column align-items-center gap-3 bg-secondary-subtle">
           <div className="image">
             <img src={men} className="img-fluid" alt="shopping item" />
           </div>
-          <div className="words">
-            <h4>MEN'S WEAR</h4>
-            <h5>12 PRODUCTS</h5>
+          <div
+            className="words text-center w-100 p-2 bg-light-subtle
+"
+          >
+            <h4 className="fs-6 fw-bolder">MEN'S WEAR</h4>
+            <h5 className="fs-6 fw-normal">12 PRODUCTS</h5>
           </div>
         </div>
-        <div className="section p-3 m-2 d-flex flex-column align-items-center gap-3 bg-secondary-subtle">
+        <div className="section p-3  d-flex flex-column align-items-center gap-3 bg-secondary-subtle">
           <div className="image">
             <img src={shoes} className="img-fluid" alt="shopping item" />
           </div>
-          <div className="words">
-            <h4>SHOES</h4>
-            <h5>15 PRODUCTS</h5>
+          <div
+            className="words text-center w-100 p-2 bg-light-subtle
+"
+          >
+            <h4 className="fs-6 fw-bolder">SHOES</h4>
+            <h5 className="fs-6 fw-normal">15 PRODUCTS</h5>
           </div>
         </div>
-        <div className="section p-3 m-2 d-flex flex-column align-items-center gap-3 bg-secondary-subtle">
+        <div className="section p-3  d-flex flex-column align-items-center gap-3 bg-secondary-subtle">
           <div className="image">
             <img src={household} className="img-fluid" alt="shopping item" />
           </div>
-          <div className="words">
-            <h4>HOUSEHOLD ITEMS</h4>
-            <h5>7 PRODUCTS</h5>
+          <div
+            className="words text-center w-100 p-2 bg-light-subtle
+"
+          >
+            <h4 className="fs-6 fw-bolder">HOUSEHOLD ITEMS</h4>
+            <h5 className="fs-6 fw-normal">7 PRODUCTS</h5>
           </div>
         </div>
-        <div className="section p-3 m-2 d-flex flex-column align-items-center gap-3 bg-secondary-subtle">
+        <div className="section p-3  d-flex flex-column align-items-center gap-3 bg-secondary-subtle">
           <div className="image">
             <img src={inner} className="img-fluid" alt="shopping item" />
           </div>
-          <div className="words">
-            <h4>INNER CLOTHES</h4>
-            <h5>35 PRODUCTS</h5>
+          <div
+            className="words text-center w-100 p-2 bg-light-subtle
+"
+          >
+            <h4 className="fs-6 fw-bolder">INNER CLOTHES</h4>
+            <h5 className="fs-6 fw-normal">35 PRODUCTS</h5>
           </div>
         </div>
-        <div className="section p-3 m-2 d-flex flex-column align-items-center gap-3 bg-secondary-subtle">
+        <div className="section p-3  d-flex flex-column align-items-center gap-3 bg-secondary-subtle">
           <div className="image">
             <img src={sports} className="img-fluid" alt="shopping item" />
           </div>
-          <div className="words">
-            <h4>SPORTS WEAR</h4>
-            <h5>16 PRODUCTS</h5>
+          <div
+            className="words text-center w-100 p-2 bg-light-subtle
+"
+          >
+            <h4 className="fs-6 fw-bolder">SPORTS WEAR</h4>
+            <h5 className="fs-6 fw-normal">16 PRODUCTS</h5>
           </div>
         </div>
       </div>
