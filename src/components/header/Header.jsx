aaad63ab309bdf249b43c2aedd4a7c722a2e13de
GabrielOchieng <ogingagabriel@gmail.com>
2024-02-22 13:52:25 +0300

    header responsiveness

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,48 @@
+// import React from "react";
+// import shopping from "../../assets/shopping.jpg";
+// import "./header.css";
+
+// const Header = () => {
+//   return (
+//     <div className="container-fluid  pt-5 mt-5 d-flex flex-column flex-lg-row flex-md-row pt-4 gap-5 align-items-center h-75 header-shop">
+//       <div className="left conatiner d-flex flex-column gap-3">
+//         <p>
+//           <span className="text-primary font-bold"> WILLYCOLLECTIONS </span>
+//           is a retail and wholesale platform that aims at providing you with
+//           quality affordable items you are at need of.{" "}
+//         </p>
+//         <p>Find all quality items you need at affordable prices</p>
+//         <h5>Delivery at your doorstep</h5>
+//         <button type="button" className="btn btn-dark fs-2">
+//           Shop Now
+//         </button>
+//       </div>
+//       <div className="right container ">
+//         <img
+//           src={shopping}
+//           className="img-fluid img-thumbnail"
+//           alt="shopping image"
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Header;
+
 import React from "react";
 import shopping from "../../assets/shopping.jpg";
 import "./header.css";
 
 const Header = () => {
   return (
-    <div className="container-fluid d-flex pt-4 gap-5 align-items-center h-75 header-shop">
-      <div className="left conatiner d-flex flex-column gap-3">
+    <div className="container-fluid header-shop d-flex flex-column flex-lg-row flex-md-row pt-5 gap-5 align-items-center h-75">
+      <div className="left container d-flex flex-column gap-3">
+        <p>
+          <span className="text-primary font-bold"> WILLYCOLLECTIONS </span>
+          is a retail and wholesale platform that aims at providing you with
+          quality affordable items you are at need of.{" "}
+        </p>
         <p>Find all quality items you need at affordable prices</p>
         <h5>Delivery at your doorstep</h5>
         <button type="button" className="btn btn-dark fs-2">
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg  fixed-top bg-primary mb-2">
+    <nav className="navbar navbar-expand-lg mb-5 fixed-top bg-light mb-2">
       <div className="container-fluid d-flex  justify-content-between align-items-center">
         <button
           className="navbar-toggler mb-2"
@@ -69,7 +69,7 @@ const Navbar = () => {
             role="search"
             className="position-relative mb-2 collapse navbar-collapse show" // Always show search form
           >
-            <IoSearch className="position-absolute searchicon cursor-pointer" />
+            <IoSearch className="position-absolute top-30  searchicon cursor-pointer" />
             <input
               className="form-control bg-body-tertiary me-2 rounded-pill ps-4 pe-5 "
               type="search"
