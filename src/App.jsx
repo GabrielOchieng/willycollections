65d1ede01d3ac6640e8b1cd82d298d0e2186443d
GabrielOchieng <ogingagabriel@gmail.com>
2024-02-22 12:42:28 +0300

    'additions '

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Category from "./components/category/Category";
 import Featured from "./components/featured/Featured";
 import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
+import Product from "./components/product/Product";
 import Testimonials from "./components/testimonials/Testimonials";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Category />
       <Featured />
+      <Product />
       <Testimonials />
     </>
   );
---
+++ src/components/product/Product.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./product.css";
+import set from "../../assets/set.jpg";
+import good from "../../assets/good.jpg";
+
+const Product = () => {
+  return (
+    <div>
+      <div className="container-fluid d-flex pt-4 gap-5 align-items-center h-75 header-shop">
+        <div className="leftp container position-relative">
+          <img
+            src={set}
+            className="img-fluid img-thumbnail "
+            alt="shopping image"
+          />
+          <div
+            className="trends position-absolute top-50 translate-middle start-50 text-white p-2 bg-black text-center
+"
+          >
+            <p className="fs-6">Latest trends</p>
+            <h3>Brand new, latest fashion designs and collections</h3>
+            <h6>Ideal for the young, old and all in between</h6>
+          </div>
+        </div>
+        <div className="rightp container flex-1 d-flex flex-column gap-2 align-items-center">
+          <img src={good} alt="" className="img-fluid" />
+          <h4 className="fs-6 fw-bolder">Ladies Dress</h4>
+          <div className="priceop d-flex align-items-center gap-1">
+            <h5 className="fs-6 fw-normal">KSH. 800</h5>
+            <button type="button" className="btn btn-dark fs-6">
+              More Options
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
