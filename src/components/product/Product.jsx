767c11fdcc27352d7d9f6882e1417d44b0cc09d3
GabrielOchieng <ogingagabriel@gmail.com>
2024-02-23 12:03:40 +0300

    changes

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -26,7 +26,6 @@ const Product = () => {
           <img src={good} alt="" className="img-fluid" />
           <h4 className="fs-6 fw-bolder">Ladies Dress</h4>
           <div className="priceop d-flex align-items-center gap-1">
-            <h5 className="fs-6 fw-normal">KSH. 800</h5>
             <button type="button" className="btn btn-dark fs-6">
               More Options
             </button>
