bc27442c850fd60489f153f7ec72ff4a18923706
GabrielOchieng <ogingagabriel@gmail.com>
2024-03-04 16:33:13 +0300

    login setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import AuthPage from "./pages/AuthPage";
 import LandingPage from "./pages/LandingPage";
 
 function App() {
+  const currentUser = true;
+
+  const RequireAuth = ({ children }) => {
+    return currentUser ? children : <Navigate to="/auth" />;
+  };
   return (
     <>
       <Routes>
-        <Route path="/" element={<LandingPage />} />
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <LandingPage />
+            </RequireAuth>
+          }
+        />
         <Route path="/auth" element={<AuthPage />} />
       </Routes>
     </>
