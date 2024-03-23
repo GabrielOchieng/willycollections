b1ce1134674e7d2a7d0c7d5aec46f8b2bd38fa9c
GabrielOchieng <ogingagabriel@gmail.com>
2024-03-23 13:55:22 +0300

    Search functionaity setup

--- src/components/itemsearchmodal/ItemSearchModal.jsx
+++ src/components/itemsearchmodal/ItemSearchModal.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const ItemSearchModal = ({ searchResults }) => {
+const ItemSearchModal = ({ searchResults, onClose }) => {
   return (
     <div className="search-results-modal position-absolute top-100 start-0 w-100">
       <div className="bg-light rounded p-3 shadow">
         <h6 className="mb-2">Search Results</h6>
+        <button
+          type="button"
+          className="btn-close float-end"
+          aria-label="Close"
+          onClick={onClose}
+        />
         {searchResults.length > 0 ? (
           <ul className="list-group">
             {searchResults.map((item) => (
-              <li key={item.id} className="list-group-item">
+              <li key={item.itemID} className="list-group-item link">
                 <Link to={`/item/${item.itemID}`}>{item.itemName}</Link>
               </li>
             ))}
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,116 +1,8 @@
-// import { IoSearch } from "react-icons/io5";
-// import { FiShoppingCart } from "react-icons/fi";
-// import { IoPersonCircleOutline } from "react-icons/io5";
-// import "./navbar.css";
-// import { useContext, useState } from "react";
-// import { Link, useParams } from "react-router-dom";
-// import { CartContext } from "../../context/CartContext";
-// import { AuthContext } from "../../context/AuthContext";
-// import ItemSearchModal from "../itemsearchmodal/ItemSearchModal";
-// import { ProductContext } from "../../context/ProductContext";
-
-// const Navbar = () => {
-//   const [isOpen, setIsOpen] = useState(false); // State variable for tracking menu visibility
-
-//   const { totalQuantity } = useContext(CartContext);
-
-//   const { currentUser } = useContext(AuthContext);
-
-//   const { items } = useContext(ProductContext);
-
-//   const toggleMenu = () => {
-//     setIsOpen(!isOpen);
-//   };
-
-//   return (
-//     <nav className="navbar navbar-expand-lg mb-5 fixed-top bg-light mb-2">
-//       <div className="container-fluid d-flex justify-content-between align-items-center">
-//         <button
-//           className="navbar-toggler mb-2"
-//           type="button"
-//           onClick={toggleMenu}
-//           data-bs-toggle={isOpen ? "collapse" : ""}
-//           data-bs-target="#navbarSupportedContent"
-//           aria-controls="navbarSupportedContent"
-//           aria-expanded={isOpen}
-//           aria-label="Toggle navigation"
-//         >
-//           {isOpen ? (
-//             <span className="close-icon">&#10006;</span> // X symbol using character code
-//           ) : (
-//             <span className="navbar-toggler-icon"></span>
-//           )}
-//         </button>
-//         <div>
-//           <Link to="/" className="navbar-brand font-bolder">
-//             WILLYCOLLECTIONS
-//           </Link>
-//         </div>
-
-//         <div
-//           className={`collapse navbar-collapse ${isOpen ? "show" : ""}`}
-//           id="navbarSupportedContent"
-//         >
-//           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-//             <li className="nav-item">
-//               <Link to="/create" className="nav-link active">
-//                 SHOP ALL
-//               </Link>
-//             </li>
-//             <li className="nav-item">
-//               <Link to="/women" className="nav-link">
-//                 WOMEN'S WEAR
-//               </Link>
-//             </li>
-//             <li className="nav-item">
-//               <Link to="/men" className="nav-link">
-//                 MEN'S WEAR
-//               </Link>
-//             </li>
-
-//             <li className="nav-item">
-//               <a href="#contact" className="nav-link">
-//                 CONTACT
-//               </a>
-//             </li>
-//           </ul>
-//         </div>
-//         <div>
-//           <form
-//             role="search"
-//             className="mb-2 border rounded-pill collapse me-2 p-1 d-flex navbar-collapse show" // Always show search form
-//           >
-//             <IoSearch className="searchicon cursor-pointer" />
-//             <input
-//               className="form-control bg-body-tertiary border-0 shadow-none "
-//               type="search"
-//               placeholder="Search for products..."
-//               aria-label="Search"
-//             />
-//           </form>
-//         </div>
-//         <div className="d-flex gap-3 align-items-center ms-0 ms-lg-4">
-//           <Link to="/cart" className="nav-link">
-//             <span className="text-danger">{totalQuantity}</span>
-//             <FiShoppingCart />
-//           </Link>
-//           <Link to="/auth" className="nav-link">
-//             {/* <span className="text-danger">{currentUser.email}</span> */}
-//             <IoPersonCircleOutline />
-//           </Link>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
+import React, { useContext, useState } from "react";
 import { IoSearch } from "react-icons/io5";
 import { FiShoppingCart } from "react-icons/fi";
 import { IoPersonCircleOutline } from "react-icons/io5";
-import "./navbar.css";
-import { useContext, useState } from "react";
+import "./navbar.css"; // Assuming your styles are in this file
 import { Link, useParams } from "react-router-dom";
 import { CartContext } from "../../context/CartContext";
 import { AuthContext } from "../../context/AuthContext";
@@ -118,10 +10,10 @@ import ItemSearchModal from "../itemsearchmodal/ItemSearchModal";
 import { ProductContext } from "../../context/ProductContext";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false); // State variable for menu visibility
-  const [searchTerm, setSearchTerm] = useState(""); // State for search term
-  const [searchResults, setSearchResults] = useState([]); // State for search results
-
+  const [isOpen, setIsOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { totalQuantity } = useContext(CartContext);
   const { currentUser } = useContext(AuthContext);
   const { items } = useContext(ProductContext);
@@ -131,16 +23,13 @@ const Navbar = () => {
   };
 
   const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value.toLowerCase()); // Lowercase for case-insensitive search
+    setSearchTerm(event.target.value.toLowerCase());
     const filteredResults = items.filter((item) =>
       item.itemName.toLowerCase().includes(searchTerm)
     );
-    console.log(items);
-    console.log(searchTerm);
     setSearchResults(filteredResults);
+    setIsModalOpen(searchTerm.length > 0); // Open modal when there's a search term
   };
-  console.log(searchResults);
-
   const handleSearchSubmit = (event) => {
     event.preventDefault(); // Prevent default form submission behavior
     // You can optionally handle form submission here, e.g., navigate to a search results page
@@ -202,20 +91,23 @@ const Navbar = () => {
           <form
             role="search"
             className="mb-2 border rounded-pill d-flex navbar-collapse show"
-            onSubmit={handleSearchSubmit} // Add form submission handler
+            onSubmit={handleSearchSubmit}
           >
             <IoSearch className="searchicon cursor-pointer" />
             <input
-              className="form-control bg-body-tertiary border-0 shadow-none "
+              className="form-control bg-body-tertiary border-0 shadow-none"
               type="search"
               placeholder="Search"
               aria-label="Search"
-              value={searchTerm} // Bind search term state to input value
-              onChange={handleSearchChange} // Update search term on input change
+              value={searchTerm}
+              onChange={handleSearchChange}
             />
           </form>
-          {searchResults.length > 0 && ( // Display search results if any
-            <ItemSearchModal searchResults={searchResults} />
+          {isModalOpen && (
+            <ItemSearchModal
+              searchResults={searchResults}
+              onClose={() => setIsModalOpen(false)}
+            />
           )}
         </div>
         <div className="d-flex gap-3 align-items-center ms-0 ms-lg-4">
