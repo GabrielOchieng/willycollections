be4b58ce10b791e3aa633d9a1074369419292b2f
GabrielOchieng <ogingagabriel@gmail.com>
2024-04-03 08:25:13 +0300

    order setup

--- src/components/ordersuccess/OrderSuccess.jsx
+++ src/components/ordersuccess/OrderSuccess.jsx
@@ -28,7 +28,7 @@ import { Link } from "react-router-dom"; // Import Link for navigation
 
 const OrderSuccess = ({ orderDetails }) => {
   return (
-    <div className="container mt-5 mb-5">
+    <div className="container mt-5 mb-5 text-center">
       <h2 className="text-center">Order Successful!</h2>
       <hr className="w-100 mb-4" />
       <div className="alert alert-success" role="alert">
